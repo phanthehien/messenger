df7b4d7cde3b82b91629e3940be3cce03529ead3
Hien Phan The <hien.phanthe@pycogroup.com>
2017-08-05 23:14:51 +0700

    add start script

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Process application/json
 app.use(bodyParser.json());
